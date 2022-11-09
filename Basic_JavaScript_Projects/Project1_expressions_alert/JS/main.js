3358a71717233153506316bbe8cbb6cda1b5b489
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2022-11-09 10:35:46 -0800

    JS Basic Project 1

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var A = "Cat", B = "Dog", C = "Squirrel"; //Assigning values to multiople variables
+window.alert(A); //Displaying an alert box with a variable
+var Sent1 = "This is the beginning of the string ", Sent2 = "and this is the end of the string"; //Assigning two more string variables
+document.write(Sent1 + Sent2); //Concatenate the Sent1 and Sent2 variables together
+document.write("<br>"); //New line
+document.write(4+6); //Displaying results of an expression
